8f37224f5811af85097c1288bcb79a3279ef7b5c
Peter Lee <peterlee@southasaigroup.com>
2020-06-09 14:49:38 +0800

    Add the database.json and generate.js for server component, also update home component.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  products = [];
+  constructor(private apiService: ApiService) { }
 
   ngOnInit(): void {
+    this.apiService.get().subscribe((data: any[]) => {
+        console.log(data);
+        this.products = data;
+    })
   }
-
 }
