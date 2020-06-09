da1d16fb7044832047a06468ff01acd5005046b9
Peter Lee <peterlee@southasaigroup.com>
2020-06-09 12:51:58 +0800

    Mocking a REST API with faker and json-server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule,
-         MatInconModule,
-         MatCardModule,
-         MatButtonModule,
-         MatProgressSpinnerModule } from '@angular/material';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';         
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [
@@ -25,7 +25,7 @@ import { MatToolbarModule,
     HttpClientModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatInconModule,
+    MatTooltipModule,
     MatCardModule,
     MatButtonModule,
     MatProgressSpinnerModule
