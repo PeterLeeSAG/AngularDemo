7eeeeb5af1374a8ec09bd983b6c0e6ab41d377d2
Peter Lee <peterlee@southasaigroup.com>
2020-06-10 20:30:44 +0800

    update templates

--- src/app/yarn-order/yarn-order.component.ts
+++ src/app/yarn-order/yarn-order.component.ts
@@ -7,10 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class YarnOrderComponent implements OnInit {
   combos = [
-    {"id":1,"name":"","description":"Incidunt et magni","price":"170.00","quantity":56840},
-    {"id":2,"name":"Rustic Concrete Chicken","description":"Sint libero mollitia","price":"302.00","quantity":9358},
-    {"id":3,"name":"Fantastic Metal Computer","description":"In consequuntur cupiditat","price":"279.00","quantity":90316},
-    {"id":4,"name":"Refined Concrete Chair","description":"Saepe nemo praesentium","price":"760.00","quantity":5899}
+    {"id":1,"code":"A","description":"Incidunt et magni","price":"170.00","quantity":56840},
+    {"id":2,"code":"B","description":"Sint libero mollitia","price":"302.00","quantity":9358},
+    {"id":3,"code":"C","description":"In consequuntur cupiditat","price":"279.00","quantity":90316},
+    {"id":4,"code":"D","description":"Saepe nemo praesentium","price":"760.00","quantity":5899}
 ];;
 
 orders = [
