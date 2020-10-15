8209782a7d90b8097189a6dd11ee2cfa2f47bfcc
Peter Lee <PeterLee@southasiagroup.com>
2020-10-15 14:48:08 +0800

    Style calculation type setting

--- src/app/yarn-order/style-item/style-item.component.ts
+++ src/app/yarn-order/style-item/style-item.component.ts
@@ -8,16 +8,32 @@ import { EventAction } from 'src/app/models/eventAction';
   styleUrls: ['./style-item.component.css']
 })
 export class StyleItemComponent implements OnInit {
+  styleName : string;
   @Input() index : number;
   @Input() style: StyleInfo;
   @Output() styleAtion = new EventEmitter<EventAction>();
+  matCalItems : {'typeID':number, 'typeText':string}[];
   event: EventAction;
 
-  constructor() { }
+  constructor() { 
+    //TODO: load the list from service class via webapi
+    this.matCalItems = []; 
+    this.matCalItems.push({'typeID':0,'typeText':"不分色組/碼數計毛"});
+    this.matCalItems.push({'typeID':1,'typeText':"分色組用毛比例計毛"});
+    this.matCalItems.push({'typeID':2,'typeText':"分碼數計毛"});
+    this.matCalItems.push({'typeID':3,'typeText':"分色組用毛比例/碼數計毛"}); 
+  }
 
   ngOnInit(): void {
     this.event = new EventAction();
     this.event.index = this.index;
+  }
+
+  ngOnChanges(changes) {
+    if (this.style != null)
+    {
+      this.styleName = this.matCalItems.find(i => i.typeID == this.style.calculationTypeId).typeText;
+    }   
   }
 
   showIndex() : number
@@ -29,6 +45,7 @@ export class StyleItemComponent implements OnInit {
   {
     //Select Style on UI
     this.event.action = "edit"
+    this.event.index = this.index;
     this.styleAtion.emit(this.event);
   }
 
@@ -36,6 +53,7 @@ export class StyleItemComponent implements OnInit {
   {
     //Remove style on UI
     this.event.action = "remove"
+    this.event.index = this.index;
     this.styleAtion.emit(this.event);
   }  
 }
